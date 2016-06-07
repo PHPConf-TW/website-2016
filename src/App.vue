c74d528de08b3c5e2291707159b6f74cd3e33104
Bificoo <bificoo@gmail.com>
2016-06-07 12:38:53 +0800

    Import foundation and add html

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,47 @@
 <template>
-  <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <Button></Button>
-    <p>
-      Welcome to your Vue.js app!
-    </p>
-    <p>
-      To get a better understanding of how this boilerplate works, check out
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">its documentation</a>.
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
-    </p>
+  <nav></nav>
+  <header>
+    <div class="banner">
+      <div class="row">
+        <h1>Banner</h1>
+      </div>
+    </div>
+  </header>
+  <div class="wrapper">
+    <Announce></Announce>
+    <Vision></Vision>
+    <Speaker></Speaker>
+    <Map></Map>
+    <Sponsor></Sponsor>
+    <Staff></Staff>
   </div>
+  <footer>
+    <div class="row">
+      2016 phpconftw
+    </div>
+  </footer>
 </template>
 
 <script lang="coffee" type="text/coffeescript">
 
   module.exports =
     components:
-      Button: require './components/Button'
+      Announce: require './components/Announce'
+      Vision: require './components/Vision'
+      Speaker: require './components/Speaker'
+      Map: require './components/Map'
+      Sponsor: require './components/Sponsor'
+      Staff: require './components/Staff'
 
 </script>
 
-<style lang="sass?indentedSyntax" type="text/sass" scoped>
+<style lang="sass?indentedSyntax" type="text/sass">
+
+  @import '~foundation-sites/scss/foundation.scss'
+  @include foundation-flex-classes
+  @include foundation-flex-grid
+
   html
     height: 100%
 
-  body
-    display: flex
-    align-items: center
-    justify-content: center
-    height: 100%
-
-  #app
-    color: #2c3e50
-    max-width: 600px
-    font-family: Source Sans Pro, Helvetica, sans-serif
-    text-align: center
-
-  #app a
-    color: #42b983
-    text-decoration: none
-
-  .logo
-    width: 100px
-    height: 100px
 </style>
---
+++ src/components/Map.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <div class="row">
+      <div class="columns">
+        <h2>Map</h2>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="coffee" type="text/coffeescript">
+</script>
+
+<style lang="sass?indentedSyntax" type="text/sass" scoped>
+</style>
