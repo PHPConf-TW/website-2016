55b52f7f44b9799f6f995002eda8713075fb6bf7
Bificoo <bificoo@gmail.com>
2016-06-13 18:10:31 +0800

    Update nav for mobile

--- src/App.vue
+++ src/App.vue
@@ -16,15 +16,6 @@
 
 <script lang="coffee" type="text/coffeescript">
   require 'foundation-sites/dist/foundation.min.js'
-#  require 'foundation-sites/js/foundation.core.js'
-#  require 'foundation-sites/js/foundation.util.keyboard.js'
-#  require 'foundation-sites/js/foundation.util.box.js'
-#  require 'foundation-sites/js/foundation.util.triggers.js'
-#  require 'foundation-sites/js/foundation.util.mediaQuery.js'
-#  require 'foundation-sites/js/foundation.util.motion.js'
-#  require 'foundation-sites/js/foundation.util.nest.js'
-#  require 'foundation-sites/js/foundation.reveal.js'
-#  require 'foundation-sites/js/foundation.accordionMenu.js'
 
   module.exports =
     data: () ->
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="menu-wrapper">
-    <div class="row align-middle">
-      <div class="shrink columns title">PHPConf Taiwan 2016</div>
-      <nav class="columns text-right">
-        <ul>
+    <div class="title-bar" data-responsive-toggle="menu" data-hide-for="medium">
+      <div class="title">PHPConf Taiwan 2016</div>
+      <button class="menu-icon" type="button" data-toggle></button>
+    </div>
+    <div class="top-bar row" id="menu">
+      <div class="top-bar-left">
+        <div class="title">PHPConf Taiwan 2016</div>
+      </div>
+      <div class="top-bar-right">
+        <ul class="menu align-right">
           <li class="active"><a href="#home">{{ menu.home }}</a></li>
           <li><a href="#announce">{{ menu.announce }}</a></li>
           <li><a href="#vision">{{ menu.vision }}</a></li>
@@ -13,7 +19,7 @@
           <li><a href="#sponsor">{{ menu.sponsor }}</a></li>
           <li><a href="#about">{{ menu.about }}</a></li>
         </ul>
-      </nav>    
+      </div>
     </div>
   </div>
 </template>
@@ -28,16 +34,34 @@
   .menu-wrapper
     background-color: $dark-gray
     color: $light-gray
+    .title-bar
+      background: inherit
+      .title
+        padding: .307rem .55rem
+    .top-bar
+      background-color: inherit
+      .title
+        +breakpoint(small only)
+          display: none
+      .menu
+        +breakpoint(small only)
+          display: block
+        li
+          +breakpoint(small only)
+            display: block
+            width: 100%
+            border-top: 1px solid #505050
+            padding: .5rem
+    .menu
+      background-color: inherit
     .title
       font-size: 1.1rem
     a
       position: relative
       display: block
       color: $light-gray
-      padding: .9rem .55rem
       &:hover
         color: $white
-
     .active a
       color: $light-gray
       background-color: inherit
@@ -48,8 +72,10 @@
         width: 100%
         height: .3rem
         background-color: $light-gray
-        top: 0
+        top: -0.5rem
         left: 0
+        +breakpoint(small only)
+          display: none
     ul
       margin: 0
       font-size: 0.9rem
