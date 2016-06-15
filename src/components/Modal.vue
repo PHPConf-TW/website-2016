f0663e330bf1d42fba1d1c213f8deaf751e35e82
Diana <louxusian111@gmail.com>
2016-06-15 09:53:41 +0800

    add modal module

---
+++ src/components/Modal.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="reveal row" id="avatarModel" data-reveal data-animation-in="fade-in" data-animation-out="fade-out">
+    <div class="photo" :style="avatarImg">
+    </div>
+    <div class="word">
+      <h5>{{ detail.name }}</h5>
+      <h6 class="lead">{{ detail.brief }}</h6>
+      <p>{{ detail.intro }}</p>
+    </div>
+    <button class="close-button" data-close aria-label="Close modal" type="button">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+</template>
+
+<script lang="coffee" type="text/coffeescript">
+  module.exports =
+    computed:
+      avatarImg: () ->
+        return 'background-image': 'url(' + @detail.photo + ')'
+    props: ['detail']
+</script>
+
+<style lang="sass?indentedSyntax" type="text/sass" scoped>
+  @import '../settings.scss'
+
+  #avatarModel
+    width: 60rem
+    height: 22rem
+    position: relative
+    overflow: hidden
+    padding: 2rem
+    +breakpoint(small only)
+      width: auto
+    .photo
+      float: left
+      margin: -2rem 1rem -1rem -2rem
+      border: 0
+      position: relative
+      background-size: cover
+      background-position: center center
+      width: 26rem
+      height: 22rem
+      display: block
+      +breakpoint(small only)
+        width: 100%
+        height: 11rem
+        margin: 2rem 0 0 0
+    .photo:after
+      content: ''
+      position: absolute
+      bottom: 0
+      right: 0
+      height: 0px
+      width: 0px
+      border-left: 3rem solid transparent
+      border-top: 11rem solid transparent
+      border-bottom: 11rem solid white
+      border-right: 3rem solid white
+      +breakpoint(small only)
+        border: 0
+    .word
+      text-align: justify
+      overflow: auto
+      height: 20rem
+      +breakpoint(small only)
+        padding-top: 2rem
+        height: auto
+        width: 100%
+        text-align: center
+    .close-button
+      color: #ddd
+      font-size: 3rem
+      right: 1.2rem
+    .close-button:hover
+      color: #ccc
+      transition: all 0.5s ease-in-out
+    h5
+      color: map-get($foundation-palette, primary)
+      font-size: 2rem
+    h6
+      font-size: 1.5rem
+    p
+      font-size: 1rem
+</style>
