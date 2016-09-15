8026998b64d7ef161767d7dfd119152007b2ea0b
Teng Yung Lin <andyNumber10@gmail.com>
2016-09-15 11:50:32 +0800

    day switch feature

--- src/components/Session.vue
+++ src/components/Session.vue
@@ -4,20 +4,20 @@
             <h4>{{ conf.menu.session }}</h4>
             <div class="dayswitch-wrapper">
                 <div class="small-6 medium-6 columns">
-                      <button type="button" class="button"><h6>Day 1 (10/29)</h6></button>
+                      <button v-on:click="changeDate(1)" type="button" class="button day1-btn"><h6>Day 1 (10/29)</h6></button>
                 </div>
                 <div class="small-6 medium-6 columns">
-                      <button type="button" class="button secondary"><h6>Day 2 (10/30)</h6></button>
+                      <button v-on:click="changeDate(2)" type="button" class="button secondary day2-btn"><h6>Day 2 (10/30)</h6></button>
                 </div>
             </div>
         </div>
-        <div class="row">
+        <div class="row day1">
             <div class="columns">
             <div class="table-scroll">
                 <table class="main-track">
                     <thead>
                         <tr>
-                            <th width="150">{{ conf.session.day1.maintrack.morning.timeplace }}</th>
+                            <th class="test" width="150">{{ conf.session.day1.maintrack.morning.timeplace }}</th>
                             <th colspan="2">{{ conf.session.day1.maintrack.morning.place }}</th>
                         </tr>
                     </thead>
@@ -59,8 +59,6 @@
                 </table>
             </div>
 
-
-                <!--  ********************************************************************************************************************************************-->
                 <table class="workshop-track">
                     <thead>
                         <tr>
@@ -84,14 +82,110 @@
                 </table>
             </div>
         </div>
+        <!--  ***********************************************day2*********************************************************************************************-->
+        <div class="row day2">
+            <div class="columns">
+            <div class="table-scroll">
+                <table class="main-track">
+                    <thead>
+                        <tr>
+                            <th class="test" width="150">{{ conf.session.day2.maintrack.morning.timeplace }}</th>
+                            <th colspan="2">{{ conf.session.day2.maintrack.morning.place }}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="session in conf.session.day2.maintrack.morning.morningtrack">
+                            <td>
+                                {{ session.time }}
+                            </td>
+                            <td colspan="2" v-if=" typeof session.content !== 'string' ">
+                                <lecture v-for="lecture in session.content" :lecture="lecture" :conf="conf"></lecture>
+                            </td>
+                            <td colspan="2" v-else>
+                                {{ session.content }}
+                            </td>
+                        </tr>
+                    </tbody>
+                <!--  ********************************************************************************************************************************************-->
+                    <thead  class=" main-track afternoon-track">
+                        <tr>
+                            <th width="14%">{{ conf.session.day2.maintrack.afternoon.timeplace }}</th>
+                            <th width="43%">{{ conf.session.day2.maintrack.afternoon.place1 }}</th>
+                            <th width="43%">{{ conf.session.day2.maintrack.afternoon.place2 }}</th>
+                        </tr>
+                    </thead>
+                    <tbody  class=" main-track afternoon-track">
+                        <tr v-for="session in conf.session.day2.maintrack.afternoon.afternoontrack">
+                            <td>
+                                {{ session.time }}
+                            </td>
+                            <td colspan="2" v-if=" typeof session.content === 'string' ">
+                                {{ session.content }}
+                            </td>
+                            <td v-else v-for="lecture in session.content">
+                                <lecture :lecture="lecture" :conf="conf"></lecture>
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+
+                <table class="workshop-track">
+                    <thead>
+                        <tr>
+                            <th width="14%">{{ conf.session.day2.workshop.timeplace }}</th>
+                            <th>{{ conf.session.day2.workshop.place }}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="session in conf.session.day2.workshop.track">
+                            <td>
+                                {{ session.time }}
+                            </td>
+                            <td v-if=" typeof session.content !== 'string' ">
+                                <lecture v-for="lecture in session.content" :lecture="lecture" :conf="conf"></lecture>
+                            </td>
+                            <td v-else>
+                                {{ session.content }}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+
     </section>
 </template>
 
 <script lang="coffee" type="text/coffeescript">
     module.exports =
         components:
-          'lecture': require './Lecture.vue'
-        props: ['session','conf']
+            'lecture': require './Lecture.vue'
+        props:
+            ['session','conf']
+        methods:
+            changeDate: (date) ->
+                if(date == 1)
+                    if(!$('.day1-btn').hasClass('active') && !$('.day1').hasClass('active'))
+                        $('.day1-btn').addClass('active')
+                        $('.day2-btn').removeClass('active')
+
+                        $('.day1').addClass('active')
+                        $('.day2').removeClass('active')
+
+                if(date == 2)
+                    if(!$('.day2-btn').hasClass('active') && !$('.day2').hasClass('active'))
+                        $('.day2-btn').addClass('active')
+                        $('.day1-btn').removeClass('active')
+
+                        $('.day2').addClass('active')
+                        $('.day1').removeClass('active')
+
+        ready: ->
+            $('.day1, .day1-btn').addClass('active')
+
 
 </script>
 
@@ -110,6 +204,21 @@
                 width: 100%
             div
                 margin-bottom: 2rem
+        .day1-btn, .day2-btn
+            opacity: 0.5
+        .day1-btn.active, .day2-btn.active
+            opacity: 1
+        .day1-btn.active
+            background: #1f41ff
+        .day2-btn.active
+            background: #ffae00
+
+
+        .day1, .day2
+            display: none
+
+        .day1.active, .day2.active
+            display: block
 
 
         .main-track
